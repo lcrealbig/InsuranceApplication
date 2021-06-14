31bc757f2bbdf316cd405bc452ded510d595964b
eryk-slowinski <e.slowinski92@gmail.com>
2021-06-14 17:30:37 +0200

    added policies table

---
+++ features/ins-3/tables/policies.sql
@@ -0,0 +1,11 @@
+create table policies(
+POLICY_NUMBER number,
+POLICY_OWNER varchar2(70),
+OWNER_ID number,
+POLICY_TYPE varchar2(50),
+POLICY_STATUS varchar2(40),
+START_DATE date,
+END_DATE date,
+PRODUCT_TYPE varchar2(40),
+ALT_POLICY_NUMBER varchar2(40)
+);
