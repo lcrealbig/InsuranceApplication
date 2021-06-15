64a7bf81ba0fd6b3da656f9ec0a431d6b03fa72d
eryk-slowinski <e.slowinski92@gmail.com>
2021-06-15 09:05:12 +0200

    added tables: cars, motorbikes, trailers, insured_objects and sequences to them

---
+++ sql/tables/insured_objects.sql
@@ -0,0 +1,10 @@
+--IN PROGRESS
+
+create table insured_objects(
+OBJECT_ID number,
+OBJECT_OWNER number,
+OBJECT_TYPE varchar2(30),
+OBJECT_POLICy_LINE number,
+OBJECT_ID number,
+OBJECT_REG_NUM varchar2(10),
+);
---
+++ sql/tables/motorbikes.sql
@@ -0,0 +1,9 @@
+create table motorbikes(
+MOTORBIKE_ID number,
+MOTORBIKE_MFG_YEAR number,     --year of manufacturing
+MOTORBIKE_BRAND varchar2(30),
+MOTORBIKE_MODEL varchar2(30),
+MOTORBIKE_ENGINE varchar2(15),
+MOTORBIKE_VIN_NUM varchar2(30),
+MOTORBIKE_POWER number
+);
