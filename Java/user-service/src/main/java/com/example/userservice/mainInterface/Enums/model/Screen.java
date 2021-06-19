64a6cb20988d79579ef1ec0231d59729c623a466
DESKTOP-O51Q7JA\mateu <mateuszparzysoffice@gmail.com>
2021-06-19 21:33:24 +0200

    Beautyfing.

--- Java/user-service/src/main/java/com/example/userservice/mainInterface/Enums/model/Screen.java
+++ Java/user-service/src/main/java/com/example/userservice/mainInterface/Enums/model/Screen.java
@@ -5,22 +5,25 @@ import java.util.Scanner;
 
 public class Screen {
     ArrayList<String> optionsToSelect = new ArrayList<>();
-    String selectedOption ="";
-    public  Screen(){}
+    String selectedOption = "";
+
+    public Screen() {
+    }
+
     public Screen(ArrayList<String> optionsToSelect) {
         this.optionsToSelect = optionsToSelect;
 
     }
-    public static int userChoice(){
+
+    public static int userChoice() {
         Scanner scn = new Scanner(System.in);
         int userChoice = scn.nextInt();
         return userChoice;
     }
-    public static boolean verify(){
+
+    public static boolean verify() {
         return true;
     }
-
-
 
 
     public ArrayList<String> getOptionsToSelect() {
--- Java/user-service/src/main/java/com/example/userservice/mainInterface/Enums/model/ScreenHandler.java
+++ Java/user-service/src/main/java/com/example/userservice/mainInterface/Enums/model/ScreenHandler.java
@@ -3,7 +3,7 @@ package com.example.userservice.mainInterface.Enums.model;
 public class ScreenHandler extends Screen{
     public static void main(String[] args) {
         Screen screen = new Screen();
-       
+
     }
 
 
