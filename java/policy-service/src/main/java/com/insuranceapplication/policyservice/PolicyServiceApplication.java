3553edf9ea65d87acad15df667eccf02bf0c0fe7
eryk-slowinski <e.slowinski92@gmail.com>
2021-06-20 16:40:42 +0200

    policy-service initial commit

---
+++ java/policy-service/src/main/java/com/insuranceapplication/policyservice/PolicyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.insuranceapplication.policyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolicyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolicyServiceApplication.class, args);
+	}
+
+}
---
+++ java/policy-service/src/test/java/com/insuranceapplication/policyservice/PolicyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insuranceapplication.policyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolicyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
