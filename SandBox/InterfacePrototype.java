ce9237d8f3a59dd063a9d3d86bd24c94e1376726
DESKTOP-O51Q7JA\mateu <mateuszparzysoffice@gmail.com>
2021-06-23 00:35:59 +0200

    Cleaning the branch from unecessary files.

--- SandBox/InterfacePrototype.java
+++
@@ -1,9 +0,0 @@
-package SandBox;
-
-public class InterfacePrototype {
-    public static void main(String[] args) {
-        System.out.println("Welcome User! Today we have :" + Utilities.ShowCurrentdate());
-        Utilities.loginVerifier();
-        Utilities.secondScreen();
-    }
-}
