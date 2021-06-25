eec7b082d5bab1ac3be3f491fa198e0905f03dde
DESKTOP-O51Q7JA\mateu <mateuszparzysoffice@gmail.com>
2021-06-25 12:20:55 +0200

    Initializing a new module - user service.

---
+++ Java/userService/src/main/java/com/example/userService/Controller.java
@@ -0,0 +1,4 @@
+package com.example.userService;
+
+public class Controller {
+}
---
+++ Java/userService/src/main/java/com/example/userService/UserServiceApplication.java
@@ -0,0 +1,14 @@
+package com.example.userService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@EnableAutoConfiguration
+@SpringBootApplication
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
