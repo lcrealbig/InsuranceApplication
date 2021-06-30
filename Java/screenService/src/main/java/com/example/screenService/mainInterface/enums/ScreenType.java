5acde8527feacae84b069e86732c607248245372
DESKTOP-O51Q7JA\mateu <mateuszparzysoffice@gmail.com>
2021-06-30 11:26:23 +0200

    Refactoring.

--- Java/screenService/src/main/java/com/example/screenService/mainInterface/enums/ScreenType.java
+++
@@ -1,5 +0,0 @@
-package com.example.screenService.mainInterface.enums;
-
-public enum ScreenType {
-    SCREEN, SEARCH , LOGIN;
-}
--- Java/screenService/src/main/java/com/example/screenService/model/policy/CreatePolicy.java
+++
@@ -1,9 +0,0 @@
-package com.example.screenService.model.policy;
-
-import com.example.screenService.model.Screen;
-
-public class CreatePolicy extends Screen {
-    public CreatePolicy() {
-        screenTitle = "Create Policy.";
-    }
-}
