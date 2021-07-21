25e6151e24a5e59933f84d37f926eb480265cef1
eryk-slowinski <e.slowinski92@gmail.com>
2021-07-21 12:29:23 +0200

    added table transactions

---
+++ sql/tables/transactions.sql
@@ -0,0 +1,13 @@
+CREATE SEQUENCE public.transactions_seq
+    INCREMENT 1
+    START 1
+    MINVALUE 1
+    MAXVALUE 999999;
+
+create table transactions(
+TRANSACTION_ID int default nextval('transactions_seq'),
+MODIFIED_BY varchar(30),
+MODIFIED_TIME DATE,
+TRANSACTION_TYPE varchar(20)
+);
+/
