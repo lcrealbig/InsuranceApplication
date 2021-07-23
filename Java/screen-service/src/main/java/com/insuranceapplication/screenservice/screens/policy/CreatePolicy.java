9e8cd08f39a06aaa1133bdccf672adf632197e3f
eryk-slowinski <e.slowinski92@gmail.com>
2021-07-23 12:21:13 +0200

    changes in create policy method: -it creates transaction object and getting its id value from db which is going to be unique for every object in db: policy, policy line and insured object -policy line is getting correct id of policy -insured object is getting correct id of policy line

--- Java/screen-service/src/main/java/com/insuranceapplication/screenservice/screens/policy/CreatePolicy.java
+++ Java/screen-service/src/main/java/com/insuranceapplication/screenservice/screens/policy/CreatePolicy.java
@@ -5,16 +5,12 @@ import com.insuranceapplication.screenservice.mainInterface.enums.RequestMethod;
 import com.insuranceapplication.screenservice.mainInterface.enums.ResponseType;
 import com.insuranceapplication.screenservice.mainInterface.enums.ScreenType;
 import com.insuranceapplication.screenservice.methods.Requests;
-import com.insuranceapplication.screenservice.models.InsuredObjectModel;
-import com.insuranceapplication.screenservice.models.PolicyLineModel;
-import com.insuranceapplication.screenservice.models.PolicyModel;
-import com.insuranceapplication.screenservice.models.VehiclesModel;
+import com.insuranceapplication.screenservice.models.*;
 import com.insuranceapplication.screenservice.screens.general.DataScreen;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Scanner;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 public class CreatePolicy extends DataScreen {
     int objectItemNo = 1;
@@ -30,52 +26,76 @@ public class CreatePolicy extends DataScreen {
         Requests.setRequestMethod(RequestMethod.POST);
         Requests.setServiceName("policy-service");
         ArrayList<String> vehicleInterface = new ArrayList<String>(Arrays.asList("vehicle type", "brand", "vehicle model", "generation", "engine type", "engine"));
-        PolicyModel policy = new PolicyModel();
-        PolicyLineModel policyLine = new PolicyLineModel();
+        TransactionModel transactionModel = new TransactionModel();
+        PolicyModel policyModel = new PolicyModel();
+        PolicyLineModel policyLineModel = new PolicyLineModel();
         InsuredObjectModel insuredObjectModel = new InsuredObjectModel();
         VehiclesModel vehicleModel = new VehiclesModel();
         Scanner userInput = new Scanner(System.in);
 
+        //creating transactions model so we can use unique transaction id for all policy information
+        transactionModel.setTransactionType("create");
+        transactionModel.setModifiedBy("Slowik");  //for now user is hardcoded, need to refactor it so we will pass current logged user
+        SimpleDateFormat sdf =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Date date = new Date();
+        transactionModel.setTimestamp(sdf.format(date));
+
+        //passing transactions to db
+        Requests.setEndpoint("createtransaction");
+        Requests.setResponseType(ResponseType.SINGLE);
+        Requests.send(transactionModel);
+        System.out.println("Please wait...");
+        pause(1000);
+
+        //getting transaction id from db
+        Long transactionId = null;
+        Requests.setEndpoint("gettransactionid");
+        JSONObject transaction = (JSONObject) Requests.send(transactionModel);
+        transactionId = (Long) transaction.get("transactionId");
+
         //setting all policy informations
+        policyModel.setTransactionId(Math.toIntExact(transactionId));
         System.out.println("Insert customer's ID: ");
-        policy.setOwner_id(userInput.nextInt());
+        policyModel.setOwnerId(userInput.nextInt());
         System.out.println("What is type of policy?");
-        policy.setType(userInput.next());
+        policyModel.setType(userInput.next());
         System.out.println("What is status of policy?");
-        policy.setStatus(userInput.next());
+        policyModel.setStatus(userInput.next());
         System.out.println("Please enter start date of policy in format YYYY-MM-DD: ");
-        policy.setStart_date(userInput.next());
+        policyModel.setStartDate(userInput.next());
         System.out.println("Please enter end date of policy in format YYYY-MM-DD: ");
-        policy.setEnd_date(userInput.next());
+        policyModel.setEndDate(userInput.next());
         System.out.println("Please enter product type: ");
-        policy.setProduct_type(userInput.next());
-        policy.setAlt_no(policy.getType() + policy.getOwner_id());
+        policyModel.setProductType(userInput.next());
+        policyModel.setAltNo(policyModel.getType() + policyModel.getOwnerId());
 
         //setting correct postrequest parameteres and sending policy object to policy service
         Requests.setEndpoint("createpolicy");
-        Requests.setResponseType(ResponseType.SINGLE);
-        Requests.send(policy);
+        Requests.send(policyModel);
         System.out.println("Please wait...");
         pause(2000);  //had to make program stop for a while, because if user types too fast it creates issues with connecting to db
 
-        //setting all policy line information
+        //getting policy no from db
         Requests.setEndpoint("getpolicy");
-        Requests.setResponseType(ResponseType.ARRAY);
-        JSONArray policyJsonArray = (JSONArray) Requests.send(policy);
-        for (int i = 0; i < policyJsonArray.size(); i++){
-            JSONObject jsonObject = (JSONObject) policyJsonArray.get(i);
-            if (jsonObject.containsKey("policy_id")){
-                Long policy_no = (Long) jsonObject.get("policy_id");
-                policyLine.setPolicy_no((policy_no).intValue());
-            }
-        }
+        Requests.send(policyModel);
+        JSONObject policy = (JSONObject) Requests.send(policyModel);
+        Long policyNo = (Long) policy.get("policyId");
+
+        //setting all policy line information
+        policyLineModel.setPolicyNo(Math.toIntExact(policyNo));
+        policyLineModel.setTransactionId(Math.toIntExact(transactionId));
         System.out.println("Please enter product line type: ");
-        policyLine.setProduct_line_type(userInput.next());
+        policyLineModel.setProductLineType(userInput.next());
 
         //setting correct postrequest parameteres and sending policyline object to policy service
         Requests.setEndpoint("createpolicyline");
-        Requests.setResponseType(ResponseType.SINGLE);
-        Requests.send(policyLine);
+        Requests.send(policyLineModel);
+
+        //getting policy line no from db
+        Requests.setEndpoint("getpolicyline");
+        Requests.send(policyLineModel);
+        JSONObject policyLine = (JSONObject) Requests.send(policyLineModel);
+        Long policyLineNo = (Long) policyLine.get("lineId");
 
         //code below is responsible for creating insured object model and sending it to policy service
         JSONArray responseArray = null;
@@ -98,27 +118,29 @@ public class CreatePolicy extends DataScreen {
                 }
                 switch (objectItemNo) {
                     case 1:
-                        vehicleModel.setVehicle_type((String) responseArray.get(userInput.nextInt() - 1));
+                        vehicleModel.setVehicleType((String) responseArray.get(userInput.nextInt() - 1));
                         break;
                     case 2:
                         vehicleModel.setBrand((String) responseArray.get(userInput.nextInt() - 1));
                         break;
                     case 3:
-                        vehicleModel.setVehicle_model((String) responseArray.get(userInput.nextInt() - 1));
+                        vehicleModel.setVehicleModel((String) responseArray.get(userInput.nextInt() - 1));
                         break;
                     case 4:
                         vehicleModel.setGeneration((String) responseArray.get(userInput.nextInt() - 1));
                         break;
                     case 5:
-                        vehicleModel.setEngine_type((String) responseArray.get(userInput.nextInt() - 1));
+                        vehicleModel.setEngineType((String) responseArray.get(userInput.nextInt() - 1));
                         break;
                     case 6:
                         vehicleModel.setEngine((String) responseArray.get(userInput.nextInt() - 1));
                         break;
                     case 7:
                         Long vehicle_id = (Long) responseArray.get(0);
-                        vehicleModel.setVehicle_id(vehicle_id.intValue());
-                        insuredObjectModel.setN01(vehicleModel.getVehicle_id());
+                        vehicleModel.setVehicleId(vehicle_id.intValue());
+                        insuredObjectModel.setN01(vehicleModel.getVehicleId());
+                        insuredObjectModel.setPolicyLineNo(Math.toIntExact(policyLineNo));
+                        insuredObjectModel.setTransactionId(Math.toIntExact(transactionId));
                         System.out.println("Please enter VIN number: ");
                         insuredObjectModel.setC01(userInput.next());
                         System.out.println("Please enter registration number: ");
