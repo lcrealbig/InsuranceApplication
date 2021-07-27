410bd4f157e8c6130cf136b19f781244c5c90477
eryk-slowinski <e.slowinski92@gmail.com>
2021-07-27 12:37:50 +0200

    creating json object with information provided from user (via inputs) and attempt to post request it

--- angular/nsureco/src/app/app.module.ts
+++ angular/nsureco/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +10,8 @@ import { UserLoginComponent } from './components/user-login/user-login.component
     UserLoginComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular/nsureco/src/app/components/user-login/user-login.component.ts
+++ angular/nsureco/src/app/components/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user-login',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
+  constructor(private http: HttpClient) { }
 
-  constructor() { }
+  url: string = `http://localhost:8083/test`; //fake api created in policy service for test purposes
+  userObj: Object;
+  userObjString: string;
+  userName: string;
+  userPassword: string;
 
-  ngOnInit(): void {
+  getUserName(userName: string) {
+    this.userName = userName;
   }
 
+  getUserPassword(userPassword: string) {
+    this.userPassword = userPassword;
+  }
+
+  createRequestBody(){
+    this.userObj = {
+      userName: this.userName,
+      userPassword: this.userPassword
+    };
+    this.userObjString = JSON.stringify(this.userObj);
+    console.log(this.userObjString);
+  }
+
+  // postData() {
+  //   this.http.post(this.url, this.userObjString).subscribe(data => {
+  //     data
+  //   });
+  // }
+
+  ngOnInit(): void {
+
+  }
 }
