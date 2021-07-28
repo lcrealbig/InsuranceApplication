3f31a05cc8bce079d4e89d3b812a8524a6292759
eryk-slowinski <e.slowinski92@gmail.com>
2021-07-28 10:44:44 +0200

    created createRequestBody method which creates body that is passed to postUser user which send post request to user service

--- angular/nsureco/src/app/components/user-login/user-login.component.ts
+++ angular/nsureco/src/app/components/user-login/user-login.component.ts
@@ -7,19 +7,16 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent implements OnInit {
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+   }
 
   url: string = `http://localhost:8083/test`; //fake api created in policy service for test purposes
   userObj: Object;
-  userObjString: string;
   userName: string;
   userPassword: string;
 
-  getUserName(userName: string) {
+  getUserData(userName: string, userPassword: string){
     this.userName = userName;
-  }
-
-  getUserPassword(userPassword: string) {
     this.userPassword = userPassword;
   }
 
@@ -28,17 +25,16 @@ export class UserLoginComponent implements OnInit {
       userName: this.userName,
       userPassword: this.userPassword
     };
-    this.userObjString = JSON.stringify(this.userObj);
-    console.log(this.userObjString);
+    this.postData();  //method is called after creating request body and sending it to user service
   }
 
-  // postData() {
-  //   this.http.post(this.url, this.userObjString).subscribe(data => {
-  //     data
-  //   });
-  // }
+  postData() {
+    this.http.post<any>(this.url, this.userObj).subscribe(data => {
+      this.userObj = data;
+      console.log(data);
+    })
+  }
 
   ngOnInit(): void {
-
   }
 }
