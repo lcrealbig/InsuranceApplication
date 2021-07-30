8f5313c702ee8e88a15e22ba75a0fe344eaaf5e2
eryk-slowinski <e.slowinski92@gmail.com>
2021-07-30 10:58:44 +0200

    initiated the user panel component (screen after succesful user login)

---
+++ angular/nsureco/src/app/components/user-panel/user-panel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'appUserPanel',
+  templateUrl: './user-panel.component.html',
+  styleUrls: ['./user-panel.component.css']
+})
+export class UserPanelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
