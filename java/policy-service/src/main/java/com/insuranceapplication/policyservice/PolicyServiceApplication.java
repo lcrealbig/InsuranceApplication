2d7daf9f49cdfd898b448884ea94e9af8a7becfe
eryk-slowinski <e.slowinski92@gmail.com>
2021-08-15 17:43:30 +0200

    refactor in imports

--- java/policy-service/src/main/java/com/insuranceapplication/policyservice/PolicyServiceApplication.java
+++ java/policy-service/src/main/java/com/insuranceapplication/policyservice/PolicyServiceApplication.java
@@ -1,12 +1,9 @@
 package com.insuranceapplication.policyservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 public class PolicyServiceApplication {
 
 	public static void main(String[] args) {
