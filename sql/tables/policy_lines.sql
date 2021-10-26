ed7ca9ae6abcf03234a49e8a12e2f6c9acd18d25
eryk-slowinski <e.slowinski92@gmail.com>
2021-10-26 15:06:07 +0200

    renamed column policy_no to policy_id

--- sql/tables/policy_lines.sql
+++ sql/tables/policy_lines.sql
@@ -7,7 +7,7 @@ create SEQUENCE public.line_id_seq
 create table policy_lines(
 POLICY_LINE_ID int default nextval('line_id_seq'),
 TRANSACTION_ID int,
-POLICY_NO int,
+POLICY_ID int,
 PRODUCT_LINE_TYPE varchar(30),
 VERSION varchar(5)
 );
