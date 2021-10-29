79f3f5ca71f30fb0a9d157a8c120c9b87d51699f
Mateusz Parzys <mateuszparzysoffice@gmail.com>
2021-10-29 22:05:54 +0200

    Added NNW risk to risks tables.

--- sql/tables/risks_headers_table.sql
+++ sql/tables/risks_headers_table.sql
@@ -1,4 +1,4 @@
-create table risks_headers (
+create table premium_calculation_config_headers (
 id serial,
 combo_id varchar(30),
 risk_id varchar(10),
@@ -18,30 +18,31 @@ header_12 varchar(30),
 version varchar(30)
 );
 --combo_id guide.
---FULL ID IS FIRST 3 CHARS FROM COMBO_NAME + L -lesser_than,B -bigger_than BE -bigger_or_equal
+--FOR OC/AC FULL ID IS FIRST 3 CHARS FROM COMBO_NAME + L -lesser_than,B -bigger_than BE -bigger_or_equal
+--FOR NNW PRC1,PRC2 ETC. STANDS FOR PROTECTION CLASS I, PROTECTION CLASS II ETC;
 
 --driver age config copy once.
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values(
 'DRI_LB','AC','driver_age','1','lesser_than','bigger_than','rise_of_premium' );
 
 --license_age config.
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values(
 'LIC_LBE','AC','license_age','1','lesser_than',
 'bigger_or_equal','rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values(
 'LIC_L','AC','license_age','1','lesser_than'
 ,'rise_of_premium' );
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values(
@@ -49,28 +50,28 @@ header_2)
 'bigger_or_equal','rise_of_premium' );
 
 --car_age config.
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values(
 'CAR_L','AC','car_age','1','lesser_than',
 'rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values('CAR_LBE',
 'AC','car_age','1','lesser_than',
 'bigger_or_equal','rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values('CAR_LBE',
 'AC','car_age','1','lesser_than',
 'bigger_or_equal','rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values('CAR_BE',
@@ -79,49 +80,95 @@ header_2)
 
 --mileage config.
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values('MIL_LBE',
 'AC','mileage','1','lesser_than',
 'bigger_or_equal','rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values('MIL_LBE',
 'AC','mileage','1','lesser_than',
 'bigger_or_equal','rise_of_premium' );
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values('MIL_BE',
 'AC','mileage','1',
 'bigger_or_equal','rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values('MIL_L',
 'AC','mileage','1','lesser_than',
 'rise_of_premium' );
 --accidents_count config.
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2,header_3)
  values('ACC_LBE',
 'AC','accidents_count','1','lesser_than',
 'bigger_or_equal','rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values('ACC_L','AC','accidents_count','1','lesser_than',
 'rise_of_premium' );
 
-insert into risks_headers
+insert into premium_calculation_config_headers
 (combo_id,risk_id,combination_name,version,header_1,
 header_2)
  values('ACC_BE','AC','accidents_count','1',
 'bigger_or_equal','rise_of_premium' );
 
+--NNW CONFIG
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1
+)
+ values('NNW_PRC1','NNW','protection_class1','1',
+'rise_of_premium' );
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1
+)
+ values('NNW_PRC2','NNW','protection_class2','1',
+'rise_of_premium' );
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1
+)
+ values('NNW_PRC3','NNW','protection_class3','1',
+'rise_of_premium' );
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1
+)
+ values('NNW_PRC4','NNW','protection_class4','1',
+'rise_of_premium' );
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1,
+header_2)
+ values('NNW_L',
+'AC','nnw_<5','1','lesser_than',
+'rise_of_premium' );
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1,
+header_2,header_3)
+ values('NNW_LBE',
+'AC','nnw_<10','1','lesser_than','bigger_or_equal',
+'rise_of_premium' );	
+
+insert into premium_calculation_config_headers
+(combo_id,risk_id,combination_name,version,header_1,
+header_2)
+ values('NNW_BE',
+'AC','nnw_>=10','1','bigger_or_equal',
+'rise_of_premium' );
