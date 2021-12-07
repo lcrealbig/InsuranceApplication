0f0ff98325752175b258822d9d414eaa71d79951
eryk-slowinski <e.slowinski92@gmail.com>
2021-12-07 14:20:00 +0100

    Created table vehicle_types_config where are defined vehicle types according to policy_line. Also added script filling this table.

---
+++ sql/scripts/vehicle_types_config_script.sql
@@ -0,0 +1,4 @@
+delete from vehicle_types_config;
+insert into vehicle_types_config (id, policy_line_id , vehicle_type, version) values ('1','MOT','MTB', '1.0');
+insert into vehicle_types_config (id, policy_line_id , vehicle_type, version) values ('2','MOT','CAR', '1.0');
+insert into vehicle_types_config (id, policy_line_id , vehicle_type, version) values ('3','TRA','TRA', '1.0');
