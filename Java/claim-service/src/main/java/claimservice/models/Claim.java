4d4ef455c8d277a6b0a54003aba677c5b5c68c56
Mateusz Parzys <mateuszparzysoffice@gmail.com>
2022-01-11 11:56:18 +0100

    Added Claim entity and model package into claim-service, sql table creating script, created Claim table in database.

---
+++ Java/claim-service/src/main/java/claimservice/models/Claim.java
@@ -0,0 +1,92 @@
+package claimservice.models;
+
+import java.util.Date;
+
+public class Claim {
+    private Integer id;
+    private String policyLineId;
+    private Integer policyId;
+    private String status;
+    private String claimType;
+    private String claimDescription;
+    private Date claimDate;
+    private String lastStatusUpdate;
+
+    public Claim() {
+    }
+
+    public Claim(Integer id, String policyLineId, Integer policyId, String status, String claimType, String claimDescription, Date claimDate, String lastStatusUpdate) {
+        this.policyLineId = policyLineId;
+        this.id = id;
+        this.policyId = policyId;
+        this.status = status;
+        this.claimType = claimType;
+        this.claimDescription = claimDescription;
+        this.claimDate = claimDate;
+        this.lastStatusUpdate = lastStatusUpdate;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getPolicyLineId() {
+        return policyLineId;
+    }
+
+    public void setPolicyLineId(String policyLineId) {
+        this.policyLineId = policyLineId;
+    }
+
+    public Integer getPolicyId() {
+        return policyId;
+    }
+
+    public void setPolicyId(Integer policyId) {
+        this.policyId = policyId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getClaimType() {
+        return claimType;
+    }
+
+    public void setClaimType(String claimType) {
+        this.claimType = claimType;
+    }
+
+    public String getClaimDescription() {
+        return claimDescription;
+    }
+
+    public void setClaimDescription(String claimDescription) {
+        this.claimDescription = claimDescription;
+    }
+
+    public Date getClaimDate() {
+        return claimDate;
+    }
+
+    public void setClaimDate(Date claimDate) {
+        this.claimDate = claimDate;
+    }
+
+    public String getLastStatusUpdate() {
+        return lastStatusUpdate;
+    }
+
+    public void setLastStatusUpdate(String lastStatusUpdate) {
+        this.lastStatusUpdate = lastStatusUpdate;
+    }
+}
