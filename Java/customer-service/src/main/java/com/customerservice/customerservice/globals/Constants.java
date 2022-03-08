275e5bc12d61af7cc98daa9bb83526a646b568d9
DESKTOP-44PMP44\Słowik <e.slowinski92@gmail.com>
2022-03-08 14:51:12 +0100

    Minor fix with configuration

--- Java/customer-service/src/main/java/com/customerservice/customerservice/globals/Constants.java
+++ Java/customer-service/src/main/java/com/customerservice/customerservice/globals/Constants.java
@@ -2,5 +2,5 @@ package com.customerservice.customerservice.globals;
 
 
 public class Constants {
-    static public final boolean isDbServiceLocal=true;
+    static public final boolean isDbServiceLocal=false;
 }
