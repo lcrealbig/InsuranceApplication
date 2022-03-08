673a8d69c419ef83fa3aadc05a8e94552a4e6c28
DESKTOP-44PMP44\Słowik <e.slowinski92@gmail.com>
2022-03-08 14:45:32 +0100

    Added required column to object_risk_config table

--- sql/scripts/object_risk_config_script.sql
+++ sql/scripts/object_risk_config_script.sql
@@ -1,11 +1,11 @@
 delete from object_risk_config;
 
 --1.0
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','OC','1.0');
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','AC','1.0');
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','NNW','1.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','OC','true','1.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','AC','false','1.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','NNW','false','1.0');
 --2.0
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','OC','2.0');
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','AC','2.0');
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','NNW','2.0');
-insert into object_risk_config (object_type, risk_id, version) values ('VEH','ASI','2.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','OC','true','2.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','AC','false','2.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','NNW','false','2.0');
+insert into object_risk_config (object_type, risk_id, required, version) values ('VEH','ASI','false','2.0');
